9c8c0dc005c80af9abec69ef6ed80705b5dae5a6
FaizMaulidin <famuld14@gmail.com>
2024-12-18 05:04:02 +0700

    produkSelect responsive

--- src/components/ProdukSelect.jsx
+++ src/components/ProdukSelect.jsx
@@ -4,7 +4,7 @@ import ImageComp from './ImageComp'
 const ProdukSelect = ({handleClick, data}) => {
   return (
       <button onClick={() => handleClick(data.id)} className='bg-cover rounded-sm hover:scale-105 transition-all duration-200 shadow-boxproduk hover:shadow-boxprodukhov bg-center relative overflow-hidden'>
-        <ImageComp src={data.image[0]} className={"absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 object-cover w-full"}/>
+        <ImageComp src={data.image[0]} className={"absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 object-cover w-full h-full"}/>
         <div className='bg-darkerblue w-full h-full bg-opacity-70 hover:bg-opacity-50 transition-all duration-300 text-white flex justify-center items-center text-2xl tracking-[0.2em] font-thin px-4 absolute top-0 left-0'>
             {data.produk}
         </div>
--- src/pages/AllProduk.jsx
+++ src/pages/AllProduk.jsx
@@ -24,18 +24,17 @@ const AllProduk = () => {
     }
 
   return (
-    <div className='font-kanit bg-darkerblue flex justify-center items-end pb-4 px-4 pt-32 w-full h-screen overflow-hidden'>
-            <PageTitle title={"Dusun Munggon - Produk"}/>
+    <div className='font-kanit bg-darkerblue flex justify-center items-end pb-4 px-4 pt-32 w-full h-screen overflow-hidden max-[480px]:overflow-y-scroll max-[480px]:h-fit max-[480px]:pt-36'>
+        <PageTitle title={"Dusun Munggon - Produk"}/>
         <Navbar/>
         <div className=' bg-darkerblue rounded-md w-full h-full overflow-hidden flex relative shadow-inner'>
-            <div className=' flex overflow-x-scroll w-full h-full justify-start p-8 scroll-custom'>
-                <div className='mr-4 bg-neutral-300 rounded-lg transition-all duration-200 shadow-boxproduk min-w-[30rem] h-full text-darkblue flex flex-col justify-center items-center pb-8 px-8'>
-                    <h1 className='text-3xl font-extralight text-center'>Produk Lokal Dusun Munggon</h1>
-                    <img className=' backdrop-saturate-200' width={280} src="./assets/produk/iconProduk.png" alt="" />
+            <div className=' flex overflow-x-scroll w-full h-full justify-start p-8 scroll-custom max-[480px]:flex-col max-[480px]:p-0 max-[480px]:gap-6 max-[480px]:overflow-visible '>
+                <div className='mr-4 bg-neutral-300 rounded-lg transition-all duration-200 shadow-boxproduk min-w-[30rem] h-full text-darkblue flex flex-col justify-center items-center pb-8 px-8 max-[480px]:min-w-full max-[480px]:py-8'>
+                    <h1 className='text-3xl font-extralight text-center max-[480px]:text-2xl'>Produk Lokal Dusun Munggon</h1>
+                    <img className=' backdrop-saturate-200 w-[280px] max-[480px]:w-48' src="./assets/produk/iconProduk.png" alt="" />
                     <p className=' font-light text-center'>Hasil dari kreativitas dan kearifan warga yang mengutamakan kualitas dalam setiap inspirasi.</p>
-                    {/* <FontAwesomeIcon className='text-[11rem]' icon={faBasketShopping}/> */}
                 </div>
-                <div className=' h-full grid grid-cols-[repeat(4,20rem)] grid-rows-2 gap-8 px-4 grid-flow-col'>
+                <div className=' h-full grid grid-cols-[repeat(4,20rem)] grid-rows-2 gap-8 px-4 grid-flow-col max-[480px]:grid-cols-2 max-[480px]:grid-rows-[repeat(4,10rem)]'>
                     {data.map((e, i) => {
                         return <ProdukSelect handleClick={handleClick} data={e} key={i}/>
                     })}
